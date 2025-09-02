bbed3c3273ac2c5cdcddee928be75fb604bec41f
João <jpmilani123@gmail.com>
2025-09-01 23:30:49 -0300

    adicionei funções

--- atividade/backend/controller/controller.js
+++ atividade/backend/controller/controller.js
@@ -33,12 +33,50 @@ export const mostrarDados = async (req, res) => {
     }
 }
 
+export const mostrarDadosEspec = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query('SELECT FROM livros WHERE id = $1', [id]);
+        res.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
 export const enviarDados = async (req, res) => {
     const { name, text } = req.body;
     try {
         await pool.query('INSERT INTO livros (name, text) VALUES ($1, $2)', [name, text]);
         res.status(200).send({
             message: "Texto salvo"
+        });
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export const deletarDados = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await pool.query('DELETE FROM livros WHERE id = $1', [id]);
+        res.status(200).send({
+            message: "Texto deletado"
+        });
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export const atualizarDados = async (req, res) => {
+    const { id } = req.params;
+    const { nome, telefone, email, idade } = req.body;
+    try {
+        await pool.query('UPDATE livros SET nome = $1, telefone = $2, email = $3, idade = $4 WHERE id = $5', [nome, telefone, email, idade, id]);
+        res.status(200).send({
+            message: "Texto atualizado"
         });
     } catch (error) {
         console.log(error);
--- atividade/backend/router/routes.js
+++ atividade/backend/router/routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { teste, criarDB, criarTBL, mostrarDados, enviarDados } from '../controller/controller.js';
+import { teste, criarDB, criarTBL, mostrarDados, enviarDados, deletarDados, atualizarDados, mostrarDadosEspec } from '../controller/controller.js';
 
 const route = Router();
 
@@ -7,6 +7,9 @@ route.get('/', teste);
 route.get('/setup/db', criarDB);
 route.get('/setup/tbl', criarTBL);
 route.get('/mostrar', mostrarDados);
+route.get('/mostrar/:id', mostrarDadosEspec);
 route.post('/enviar', enviarDados);
+route.put('/alterar/:id', atualizarDados);
+route.delete('/deletar/:id', deletarDados);
 
 export default route;
